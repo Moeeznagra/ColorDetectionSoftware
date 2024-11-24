a265bcca5a34935fb2dc8473d0e04bff0f7d0373
moeeznagra <ufx876@usask.ca>
2024-11-24 15:36:57 -0600

    Initial Setup

---
+++ main.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+from PIL import Image, ImageTk
+from colorthief import ColorThief
+import os
+
+
+root=Tk()
+root.title("Color Detection from image")
+root.geometry("800x470+100+100")
+root.configure(bg="#e4e8eb")
+root.resizable(False, False)
+
+
+#icon
+image_icon = PhotoImage(file="icon.png")
+root.iconphoto(False, image_icon)
+
+
+root.mainloop()
